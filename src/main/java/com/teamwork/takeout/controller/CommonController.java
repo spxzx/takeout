51e4873bf84ecaaa694ba46186c50b396071eacf
spxzx <930636419u@gmail.com>
2022-09-20 20:18:56 +0800

    实现图片上传下载功能

---
+++ src/main/java/com/teamwork/takeout/controller/CommonController.java
@@ -0,0 +1,60 @@
+package com.teamwork.takeout.controller;
+
+import com.teamwork.takeout.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/common")
+public class CommonController {
+
+    @Value("${takeout.path}")
+    private String basePath;
+
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String fileName = UUID.randomUUID().toString() + suffix;
+        File dir = new File(basePath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        try {
+            file.transferTo(new File(basePath + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return R.success(fileName);
+    }
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse resp) {
+        try {
+            FileInputStream fis = new FileInputStream(new File(basePath + name));
+            ServletOutputStream sos = resp.getOutputStream();
+            resp.setContentType("image/jpeg");
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fis.read(bytes)) != -1) {
+                sos.write(bytes,0,len);
+                sos.flush();
+            }
+            sos.close();
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/teamwork/takeout/filter/LoginCheckFilter.java
+++ src/main/java/com/teamwork/takeout/filter/LoginCheckFilter.java
@@ -42,7 +42,8 @@ public class LoginCheckFilter implements Filter {
                 "/employee/login",
                 "/employee/logout",
                 "/backend/**",
-                "/front/**"
+                "/front/**",
+                "/common/**"
         };
 
         if (check(urls, reqURI)){
