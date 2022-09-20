99f1265497cfb5b97a2632b96f28f9816e42b715
spxzx <930636419u@gmail.com>
2022-09-20 09:20:43 +0800

    实现登录功能

---
+++ src/main/java/com/teamwork/takeout/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.teamwork.takeout.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.teamwork.takeout.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
