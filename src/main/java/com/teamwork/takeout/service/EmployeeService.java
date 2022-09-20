3f1fadf5699c4cb2999305e1ce33def080e82d47
spxzx <110330125+spxzx@users.noreply.github.com>
2022-09-20 10:18:53 +0800

    Delete src/main/java/com/teamwork/takeout/service directory

--- src/main/java/com/teamwork/takeout/service/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package com.teamwork.takeout.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.teamwork.takeout.entity.Employee;
-
-public interface EmployeeService extends IService<Employee> {
-}
